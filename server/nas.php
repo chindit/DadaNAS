618ce4dd1e17c981e002be44d292a82e13b4a79e
David Lumaye <chindit@users.noreply.github.com>
2016-05-24 17:27:04 +0200

    Create nas.php

---
+++ server/nas.php
@@ -0,0 +1,24 @@
+<?php
+if(isset($_GET['action'])){
+	$json = array();
+        switch($_GET['action']){
+                case 'noshutdown':
+			$file = fopen('tmp/noshutdown', 'w');
+                        $result = fclose($file);
+			$json['result'] = $result; 
+                break;
+		case 'shutdown':
+			$result = true;
+			if(file_exists('tmp/noshutdown')){
+				$result = unlink('tmp/noshutdown');
+			}
+			$json['result'] = $result;
+		break;
+		default:
+			echo json_encode(array('result' => false));
+                break;
+        }
+	//Dans tous les cas, on affiche le status
+	$json['shutdown'] = file_exists('tmp/noshutdown');
+	echo json_encode($json);
+}
