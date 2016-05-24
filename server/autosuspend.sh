60b39becb3dc713954cb00d1c419dc7cd02702e7
David Lumaye <chindit@users.noreply.github.com>
2016-05-24 22:40:41 +0200

    Fix uptime bug

--- server/autosuspend.sh
+++ server/autosuspend.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-date=`date +%Y-%m-%d:%H:%M:%S`
 has_ssh=`netstat -ant | grep LOCAL_IP:PORT | grep ESTABLISHED`
 has_samba=`smbstatus | grep -c '[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*'`
-uptime=`uptime | awk '{print $3}'`
+uptime=`uptime -s`
+dateCal=`date --date "$uptime" +%s`
+date=`date +%s`
 
 if [ "x$has_ssh" = "x" ];
 then
@@ -12,15 +13,15 @@ then
     then
         echo $date " : No shutdown - FILE" >> "/tmp/log.txt"
     else
-	if [ $has_samba -gt 0 ];
-	then
-	    echo $date " : No shutdown - SHARE" >> "/tmp/log.txt"
-	else
-	    if [ $uptime -gt 10 ];
-	    then
-            	systemctl hibernate
-	    fi
-	fi
+        if [ $has_samba -gt 0 ];
+        then
+            echo $date " : No shutdown - SHARE" >> "/tmp/log.txt"
+        else
+            if [ $date -gt $(( $dateCal + 600 )) ];
+            then
+                systemctl hibernate
+            fi
+        fi
     fi
 else
     echo $date " : No shutdown - SSH" >> "/tmp/log.txt"
